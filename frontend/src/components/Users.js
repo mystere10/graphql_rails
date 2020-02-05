5eb7c04b76ddc8afee486865fbcfbd3bd259b7ed
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2020-02-05 16:31:19 +0200

    added create user

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import UserAvatar from './UserAvatar';
-// import CreateUser from './CreateUser'
+import CreateUser from './CreateUser'
 
-const GET_USER = gql`{
+const GET_USERS = gql`{
   users{
     id
     name
@@ -14,11 +14,18 @@ const GET_USER = gql`{
 }`;
 
 function Users({ selectUser }){
-  const {loading, error, data} = useQuery(GET_USER);
+  const {loading, error, data} = useQuery(GET_USERS);
 
   if (loading) return 'loading...';
   if (error) return `Error ${error.message}`;
 
+  function updateUsers(cache, {data: {createUser}}){
+    const {users} = cache.readQuery({query: GET_USERS});
+    cache.writeQuery({
+      query: GET_USERS,
+      data: {users: users.concat([createUser.user])}
+    })
+  }
   return(
     <div className="flex flex-wrap items-center pb-16">
       {data.users.map(user => (
@@ -26,6 +33,7 @@ function Users({ selectUser }){
           <UserAvatar user={user}/>
         </div>
       ))}
+    <CreateUser onCreateUser={updateUsers}/>
     </div>
   )
 }
